fef5c7a51df0153983d7c1e0d3621b3846fd194a
Augusto Avelino <augusto_avelino@icloud.com>
2024-04-01 16:29:14 -0300

    Adds more regex test cases

--- regexyTests/RegexEvaluatorTests.swift
+++ regexyTests/RegexEvaluatorTests.swift
@@ -32,11 +32,32 @@ final class RegexEvaluatorTests: XCTestCase {
         XCTAssertEqual(letterMatches.count, 6)
     }
     
+    func testRegexEvaluatorFindsCorrectMatches() throws {
+        let content = "Hello world"
+        regex.setPattern("[A-z]\\w+")
+        let wordMatches = try regex.matches(in: content)
+        XCTAssertEqual(wordMatches.count, 2)
+        
+        let firstMatch = wordMatches[0]
+        let firstWord = String(content[firstMatch.range])
+        XCTAssertEqual(firstWord, "Hello")
+        
+        let secondMatch = wordMatches[1]
+        let secondWord = String(content[secondMatch.range])
+        XCTAssertEqual(secondWord, "world")
+    }
+    
+    func testRegexEvaluatorFindsRanges() throws {
+        regex.setPattern("[A-z]")
+        let letterRanges = try regex.ranges(in: "abcdef")
+        XCTAssertEqual(letterRanges.count, 6)
+    }
+    
     func testRegexEvaluatorFindsCorrectRanges() throws {
-        let content = "Testing words"
+        let content = "Testing more words now"
         regex.setPattern("[A-z]\\w+")
         let wordRanges = try regex.ranges(in: content)
-        XCTAssertEqual(wordRanges.count, 2)
+        XCTAssertEqual(wordRanges.count, 4)
         
         let firstRange = wordRanges[0]
         let firstWord = String(content[firstRange.lowerBound..<firstRange.upperBound])
@@ -44,13 +65,15 @@ final class RegexEvaluatorTests: XCTestCase {
         
         let secondRange = wordRanges[1]
         let secondWord = String(content[secondRange.lowerBound..<secondRange.upperBound])
-        XCTAssertEqual(secondWord, "words")
-    }
-    
-    func testRegexEvaluatorFindsRanges() throws {
-        regex.setPattern("[A-z]")
-        let letterRanges = try regex.ranges(in: "abcdef")
-        XCTAssertEqual(letterRanges.count, 6)
+        XCTAssertEqual(secondWord, "more")
+        
+        let thirdRange = wordRanges[2]
+        let thirdWord = String(content[thirdRange.lowerBound..<thirdRange.upperBound])
+        XCTAssertEqual(thirdWord, "words")
+        
+        let fourthRange = wordRanges[3]
+        let fourthWord = String(content[fourthRange.lowerBound..<fourthRange.upperBound])
+        XCTAssertEqual(fourthWord, "now")
     }
     
     func testRegexEvaluatorThrowsMatchesErrorForWrongPattern() throws {
